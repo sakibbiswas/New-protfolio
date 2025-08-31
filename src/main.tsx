1fb082ab61f59cdb8eaa280558a4a02ec4358834
Sakib Biswas <sakibsakib99880@gmail.com>
2025-08-31 17:12:40 +0600

    skills  add

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './Componets/Routes/Routes';
-// import { router } from './Components/Routes/Routes';
-
+import { router } from './Componets/Routes/Routes'
 // TypeScript-safe way to get root element
 const rootElement = document.getElementById('root');
 
